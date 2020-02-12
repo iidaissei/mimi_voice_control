a8dcaf5699108c474bc2ed06b029dbcdf640ae8a
iidaissei <issei.288.higashi@icloud.com>
2020-02-13 00:11:02 +0900

    2020/02/13

--- src/sm_voice_control.py
+++ src/sm_voice_control.py
@@ -109,7 +109,7 @@ class STask(smach.State):
             else:
                 speak('fuck')
         elif userdata.cmd_input == 'start happy time':
-            speak("Welcome to Tokyo Disneyland !!")
+            speak("Welcome to Tokyo DisneySea")
             bgmPlay('happy_time.mp3')
         elif userdata.cmd_input == 'finish voice control':
             playMessage('goodbye.wav')

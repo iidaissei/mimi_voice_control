9f625da0653b1d7c924bafabea81eb27e89097e1
iidaissei <issei.288.higashi@icloud.com>
2020-02-12 11:20:47 +0900

    2020/02/12

--- src/vc_common_function.py
+++ src/vc_common_function.py
@@ -115,3 +115,28 @@ def playMessage(wav_name):
         data = wf.readframes(chunk)
     stream.close()
     p.terminate()
+
+
+def playMusic(wav_name):
+    try:
+        wf = wave.open(
+        '/home/athome/catkin_ws/src/mimi_voice_control/wav_file/'+ wav_name, "r")
+    except FileNotFoundError:
+        print("[Error 404] No such file")
+
+    # ストリームを開く
+    p = pyaudio.PyAudio()
+    stream = p.open(
+            format = p.get_format_from_width(wf.getsampwidth()),
+            channels = wf.getnchannels(),
+            rate = wf.getframerate(),
+            output = True)
+
+    # 音声を再生
+    # chunk = 1024
+    data = wf.readframes(chunk)
+    while data != '':
+        stream.write(data)
+        data = wf.readframes(chunk)
+    # stream.close()
+    # p.terminate()
